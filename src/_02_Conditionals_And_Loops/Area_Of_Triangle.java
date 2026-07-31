bf30b7860416e1c366ef95420b9d749ac88b2781
notGUMMY <rameshsuri21@gamil.com>
2026-07-31 21:50:56 +0530

    DSA

---
+++ src/_02_Conditionals_And_Loops/Area_Of_Triangle.java
@@ -0,0 +1,13 @@
+package _02_Conditionals_And_Loops;
+import java.util.Scanner;
+public class Area_Of_Triangle {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Base: ");
+        float Base = input.nextFloat();
+        System.out.print("Enter Height: ");
+        float Height = input.nextFloat();
+        double Ans = (float) (0.5*Base*Height);
+        System.out.print("Area of Triangle: " + Ans);
+    }
+}
